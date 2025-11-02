2cf32325227d8cbdf04c95b74c06484031381d4b
KattaChad <adityasamal306@gmail.com>
2025-11-02 18:06:29 +0530

    Added front-end service

---
+++ watchlist-frontend/src/main/java/com/example/watchlist_frontend/WatchlistFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchlist_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlistFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchlistFrontendApplication.class, args);
+	}
+
+}
